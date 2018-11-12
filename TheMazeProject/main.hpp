30dfce46c76711c82a80085e7d9a5db9633cb3c5
Umut Soysal <us.soysal@gmail.com>
2018-11-12 11:59:26 -0500

    Commit alltogether

---
+++ TheMazeProject/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  TheMazeProject
+//
+//  Created by usoysal on 11/12/18.
+//  Copyright © 2018 usoysal. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
