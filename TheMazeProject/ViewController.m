eb3f545a28fa08ee5d3d1427a784fef83e9e42d5
Umut Soysal <us.soysal@gmail.com>
2018-11-12 11:55:54 -0500

    Initial Commit

---
+++ TheMazeProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TheMazeProject
+//
+//  Created by usoysal on 11/12/18.
+//  Copyright © 2018 usoysal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
---
+++ TheMazeProjectTests/TheMazeProjectTests.m
@@ -0,0 +1,37 @@
+//
+//  TheMazeProjectTests.m
+//  TheMazeProjectTests
+//
+//  Created by usoysal on 11/12/18.
+//  Copyright © 2018 usoysal. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TheMazeProjectTests : XCTestCase
+
+@end
+
+@implementation TheMazeProjectTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
